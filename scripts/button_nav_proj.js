ce77f085e8649b2823665e9b667c25b2d347233a
MtotMtot <126000669+MtotMtot@users.noreply.github.com>
2025-02-10 13:53:51 +0000

    script declaration moved to button_nav_proj
    old button nav proj script code commented out
    scripts from projects page moved to this script.

--- scripts/button_nav_proj.js
+++ scripts/button_nav_proj.js
@@ -2,6 +2,63 @@
     This script handles the content assignment of project data
 */
 
+
+
+function LoadProject1() {
+    var currently = document.getElementById('info').getAttribute('style');
+    var currently = document.getElementById('info1').getAttribute('style');
+    if (currently != 'display:none') {
+        document.getElementById('info1').setAttribute('style', 'display:none');
+    }
+    else {
+        document.getElementById('info1').setAttribute('style', 'display:block');
+    }
+}
+
+function LoadProject2() {
+    var currently = document.getElementById('info').getAttribute('style');
+    var currently = document.getElementById('info2').getAttribute('style');
+    if (currently != 'display:none') {
+        document.getElementById('info2').setAttribute('style', 'display:none');
+    }
+    else {
+        document.getElementById('info2').setAttribute('style', 'display:block');
+    }
+}
+
+function LoadProject3() {
+    var currently = document.getElementById('info').getAttribute('style');
+    var currently = document.getElementById('info3').getAttribute('style');
+    if (currently != 'display:none') {
+        document.getElementById('info3').setAttribute('style', 'display:none');
+    }
+    else {
+        document.getElementById('info3').setAttribute('style', 'display:block');
+    }
+}
+
+function LoadProject4() {
+    var currently = document.getElementById('info').getAttribute('style');
+    var currently = document.getElementById('info4').getAttribute('style');
+    if (currently != 'display:none') {
+        document.getElementById('info4').setAttribute('style', 'display:none');
+    }
+    else {
+        document.getElementById('info4').setAttribute('style', 'display:block');
+    }
+}
+
+function LoadProject5() {
+    var currently = document.getElementById('info5').getAttribute('style');
+    if (currently != 'display:none') {
+        document.getElementById('info5').setAttribute('style', 'display:none');
+    }
+    else {
+        document.getElementById('info5').setAttribute('style', 'display:block');
+    }
+}
+
+/*      /// OLD PROJECT NAV CODE.
 const ProjectNames = 
 [
     "api",
@@ -16,7 +73,7 @@ let ProjectText = [];
 
 let projTextArea = document.getElementById("project_data_area");
 
-/* Adds the included text data into the array of text. */
+/// Adds the included text data into the array of text. 
 for(let i = 0; i < ProjectNames.length; ++i)
 {
     ProjectText.push("");
@@ -54,3 +111,4 @@ function LoadProject(button, btnIndex)
         document.getElementById(button.getAttribute("id")).className = "proj-button-active";
     }
 }
+*/
