d2f458aba9137267135f85c77fab4a744d91be6b
MtotMtot <louisgbaker@hotmail.com>
2025-02-25 22:33:21 +0000

    remove slider stuff (not good)

--- javascript/beforeAfter.js
+++
@@ -1,4 +0,0 @@
-const container = document.querySelector('.slider-container');
-document.querySelector('.slider').addEventListener('input', (e) => {
-    container.style.setProperty('--position', `${e.target.value}%`);
-})
